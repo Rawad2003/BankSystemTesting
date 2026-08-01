e5185d8358e8a516c55f0336e7af4a6d4f63df39
Rawad2003 <rawad_isayed@yahoo.com>
2026-08-01 14:26:49 +0300

    setup DataProvider at the beginning

--- src/test/java/com/project/bank/system/login/LoginTest.java
+++ src/test/java/com/project/bank/system/login/LoginTest.java
@@ -1,11 +1,17 @@
 package com.project.bank.system.login;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.project.bank.system.base.BaseSetup;
 
 public class LoginTest extends BaseSetup {
+	@DataProvider
+	public static Object[] loginData() {
+		return new Object[][] { { "Rawad", "pass123" } };
+	}
+
 	@Test
 	public void loginTesting() {
 		loginPage page = new loginPage(driver);

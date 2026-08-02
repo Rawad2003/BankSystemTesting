c16b7239352950075941f4f230eb1c997f5ce385
Rawad2003 <rawad_isayed@yahoo.com>
2026-08-02 21:32:30 +0300

    setting the locators of Register

--- src/main/java/com/project/bank/system/login/registerPage.java
+++ src/main/java/com/project/bank/system/login/registerPage.java
@@ -8,4 +8,12 @@ public class registerPage {
 	By lastName = By.id("customer.lastName");
 	By street = By.id("customer.address.street");
 	By city = By.id("customer.address.city");
+	By state = By.id("customer.address.state");
+	By zipCode = By.id("customer.address.zipCode");
+	By phoneNumber = By.id("customer.phoneNumber");
+	By ssn = By.id("customer.ssn");
+	By userName = By.id("customer.username");
+	By password = By.id("customer.password");
+	By confirmPassword = By.id("repeatedPassword");
+	By registerButton = By.xpath("//input[@value='Register']");
 }

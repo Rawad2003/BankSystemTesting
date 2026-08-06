17b98578d6413f02bffa3d7a852be1bce0a4bd24
Rawad2003 <rawad_isayed@yahoo.com>
2026-08-06 19:27:16 +0300

    Update homePage.java

--- src/main/java/com/project/bank/system/homepage/homePage.java
+++ src/main/java/com/project/bank/system/homepage/homePage.java
@@ -1,12 +1,17 @@
 package com.project.bank.system.homepage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class homePage {
+	private String URL = "https://parabank.parasoft.com/parabank/index.htm";
+	private WebDriver driver;
+
 	By ParaBankLogo = By.xpath("//img[@alt='ParaBank']");
 	By userName = By.name("username");
 	By password = By.name("password");
 	By loginButton = By.xpath("//input[contains(@value,'Log')]");
 	By forgotLoginInfo = By.linkText("Forgot login info?");
 	By registerLink = By.linkText("Register");
+	By latestNews;
 }
